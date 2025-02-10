eb212fd0a68f85c884e131f94836348b64016042
Esther <esywacu@gmail.com>
2025-02-10 21:51:32 +0000

    changes templates

--- todo/forms.py
+++ todo/forms.py
@@ -1,19 +1,18 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 from .models import Task, Child
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'priority', 'status', 'due_date', 'child']
+
+class ChildForm(forms.ModelForm):
+    class Meta:
+        model = Child
+        fields = ['name', 'age']
 
 class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']
-
-class TaskForm(forms.ModelForm):
-    class Meta:
-        model = Task
-        fields = ['title', 'description', 'child']
-
-class ChildForm(forms.ModelForm):
-    class Meta:
-        model = Child
-        fields = ['name']
